3a6e488aab26735f7cb3272a0dfec756d2c93057
addeym <addey.mugtaar@hva.nl>
2024-06-04 16:49:08 +0200

    <mogelijke opmaak samen bedacht>

--- rail.py
+++ rail.py
@@ -1,22 +1,31 @@
 import csv
 
 
-class trein():
-    def __init__(self, naam):
-        self.name = naam
-        self.bezocht_stations = []
+class traject():
+    def __init__(self, start_station):
+        self.verbinding = []
+        self.reistijd = 0 
+        eind_station = start_station
+        
+
+    
+    
 
 class station():
     def __init__(self, naam):
-        self.name = naam
-
-class tijd():
-    def __init__(self, start,eind):
-        self.start = start
-        self.eind = eind 
+    self.name = naam  
+    # met welke stations de station verbinding heeft
+    self.verbinding = []                                                                                                                                                                                                              
 
 
 
+def functie(station):
+    if station heeft verbinding met eind station
+        voeg station aan lijst verbindingen 
+        tel tijd bij 
+        update eind station 
+
+    
 
 
 
@@ -25,6 +34,19 @@ with open('StationsHolland.csv', newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
 
     for row in csvreader:
-        print(row[0])
+        
+        maak station.naam aan 
+        stop alle stations.namen in dictionary 
+        #print(row[0])
+
+with open('ConnectiesHolland.csv', newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    minuten = 0
+    for row in csvreader:
+        als station.naam  gelijk is aan row[0]
+            add row[1] in lijst verbindingen 
+        minuten += int(row[2])
+        print(row[2])
+    print(minuten)
     
     
