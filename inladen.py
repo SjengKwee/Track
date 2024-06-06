63fe29433379c1fc4a3f39588ff2b3c05158d70f
Alec <alecvanduin@DESKTOP-R25CRHQ>
2024-06-06 12:54:25 +0200

    Inladen/verbindingen af

---
+++ inladen.py
@@ -0,0 +1,35 @@
+# Aangemaakt door Addey
+# Inladen.py
+# Hier laden we de data in, koel
+
+import csv
+from Station import *
+
+def stations_aanmaken():
+    """
+    Laad alle stations in, returnt een dictionary met alle lege stations
+    """
+    with open('StationsHolland.csv', newline='') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=',')
+        stations = {}
+        for row in csvreader:
+            newStation = Station(row[0])
+            stations[row[0]] = newStation
+        
+    return stations
+
+
+def verbinding():
+    """
+    Maakt alle stations met hun verbindingen, met behulp van stations_aanmaken(), returnt een dictionary
+    """
+    with open('ConnectiesHolland.csv', newline='') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=',')
+        stations = stations_aanmaken()
+        for row in csvreader:
+            if (row[0] in stations) & (row[1] in stations):
+                stations[row[0]].add_verbinding(stations[row[1]],row[2])
+                stations[row[1]].add_verbinding(stations[row[0]], row[2])
+        return stations
+                    
+st = verbinding()
--- rail.py
+++ rail.py
@@ -6,7 +6,7 @@ class traject():
         self.verbinding = []
         self.reistijd = 0 
         eind_station = start_station
-        
+
 class station():
     def __init__(self, naam):
     self.name = naam  
