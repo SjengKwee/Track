19d147599317adc5ebf6ff9e84d602067f8cf0a7
addeym <addey.mugtaar@hva.nl>
2024-06-07 11:21:10 +0200

    plot functioneerd

--- plotten.py
+++ plotten.py
@@ -9,20 +9,20 @@ from inladen import make_stations
 stations = make_stations()
 
 station_names = list(stations.keys())
-y = [stations[station]._coordinates[0] for station in station_names]
-x = [stations[station]._coordinates[1] for station in station_names]
+y = [float(stations[station]._coordinates[0]) for station in station_names]
+x = [float(stations[station]._coordinates[1]) for station in station_names]
 #print(st["Hoorn"]._coordinates[0])
 #print(stations["Hoorn"]._x)
 
 
-plt.figure(figsize=(10, 15))
+plt.figure(figsize=(15, 20))
 plt.scatter(x, y)
 
 for i, name in enumerate(station_names):
     plt.text(x[i], y[i], name, fontsize=9)
     
-plt.grid(True)
-
+# plt.grid(True)
+plt.savefig('images/plot_zuid_noord.png') 
 
 plt.show()
 
