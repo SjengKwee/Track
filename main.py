6278c45b6dd6b17f25fc9b4aa9d98b677cc517a4
Alec <alecvanduin@DESKTOP-R25CRHQ>
2024-06-11 12:17:23 +0200

    Min en max random toegevoegd

--- main.py
+++ main.py
@@ -5,8 +5,26 @@ from bouwblokjes.score import *
 
 stations = make_connections()
 score_list = []
-for i in range(100000):
+max_score = 0
+min_score = 10000
+
+for i in range(10000):
     traj = run_random_algoritme(stations)
     score_list.append(score_calc(traj))
+    if score_calc(traj) > max_score:
+        max_score = score_calc(traj)
+        max_traj = traj
+    elif score_calc(traj) < min_score:
+        min_score = score_calc(traj)
+        min_traj = traj
+
+print(max_score, min_score)
+print("Het beste traject is: ")
+for tracks in max_traj:
+    print(tracks._stations)
+    
+print("Het slechtste traject is: ")
+for tracks in min_traj:
+    print(tracks._stations)
 
 run_plot_random_alg_score(score_list)
