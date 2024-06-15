ef16af1397a77e6844657d022a860af7c01dd9e5
Alec <alecvanduin@DESKTOP-R25CRHQ>
2024-06-15 13:59:09 +0200

    Main aangepast, input word opgevraagd voor welk algoritme we runnen

--- main.py
+++ main.py
@@ -4,43 +4,34 @@ from code.bouwblokjes.plot_trajectories import *
 from code.bouwblokjes.inladen import *
 from code.bouwblokjes.score import *
 from code.bouwblokjes.writer import *
+import sys
 
 stations = make_connections()
 
-#random_results = run_random_times(stations, 1000)
-restr_results = run_random_restr_times(stations, 10000)
+print("Welkom bij dit vette ")
+print("Om onze random baseline te runnen: Random")
+print("Om de restricted random nr 1 te runnen: Restricted_1")
+algoritme = input("Welk algoritme wil je runnen?")
 
-print(restr_results[1])
-print(restr_results[2])
-print(restr_results[3])
-print(restr_results[4])
+#Random algoritme
+if(algoritme == "Random"):
+    random_results = run_random_times(stations, 10000)
+    print("Het algoritme duurt", random_results[5], "seconden")
+    run_plot_random_alg_score(random_results[0], 'data/images/baseline/scores_random_algoritme')
+    tracks_writer(random_results[2], random_results[1], 'data/output/baseline/maximum.csv')
+    tracks_writer(random_results[4], random_results[3], 'data/output/baseline/minimum.csv')
+    run_plot_trajectories('data/output/baseline/maximum.csv', 'data/images/baseline/max_7_track.png')
+    run_plot_trajectories('data/output/baseline/minimum.csv', 'data/images/baseline/min_7_track.png')
 
-# print(random_results[1], random_results[3])
-# print("Het beste traject is: ")
-# for tracks in random_results[2]:
-#     print(tracks._stations)
+#Restricted nr 1
+elif(algoritme == "Restricted_1"):
+    restr1_results =run_random_restr_times(stations,10000)
+    print("Het algoritme duurt", restr1_results[5], "seconden")
+    run_plot_random_alg_score(restr1_results[0], 'data/images/random_restricted_1/scores_random_algoritme')
+    tracks_writer(restr1_results[2], restr1_results[1], 'data/output/random_restricted_1/maximum.csv')
+    tracks_writer(restr1_results[4], restr1_results[3], 'data/output/random_restricted_1/minimum.csv')
+    run_plot_trajectories('data/output/random_restricted_1/maximum.csv', 'data/images/random_restricted_1/max_7_track.png')
+    run_plot_trajectories('data/output/random_restricted_1/minimum.csv', 'data/images/random_restricted_1/min_7_track.png')
 
-# print("Het slechtste traject is: ")
-# for tracks in random_results[4]:
-#     print(tracks._stations)
-
-# print("Het algoritme duurt", random_results[5], "seconden")
-# run_plot_random_alg_score(random_results[0], 'data/images/scores3_random_algoritme')
-
-
-# # write output for maximum score
-# tracks_writer(random_results[2], random_results[1], 'data/output/maximum3.csv')
-
-# # write output for minimum score
-# tracks_writer(random_results[4], random_results[3], 'data/output/minimum3.csv')
-# tracks_writer(random_results[6],random_results[7], 'data/output/medium3.csv')
-
-# # plot graph from written max score
-# run_plot_trajectories('data/output/maximum3.csv', 'data/images/max_7_track3.png')
-
-# # plot graph from written min score
-# run_plot_trajectories('data/output/minimum3.csv', 'data/images/min_7_track3.png')
-
-# run_plot_trajectories('data/output/medium3.csv', 'data/images/medium_7_track3.png')
-
-
+else:
+    print("Verkeerde input")
