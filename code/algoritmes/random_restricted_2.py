82a5d71838a615c152f52324adbd01b5d9113424
Alec <alecvanduin@DESKTOP-R25CRHQ>
2024-06-15 17:26:19 +0200

    [Alec] Beta versie random restricted 2 toegevoegd

---
+++ code/algoritmes/random_restricted_2.py
@@ -0,0 +1,79 @@
+# Aangemaakt door Alec van Duin
+# Random_algoritme_2.py
+# Hier definieren we een restricted random algoritme, waar tracks niet twee keer door dezelfde verbinding gaat
+
+class Restricted_2():
+
+    def __init__(self, stations: dict):
+        self._stations = copy.deepcopy(stations)
+    
+    def random_restr2_traject(self):
+        #Initialiseer parameters
+        start_station = self._stations[random.choice(list(self._stations.keys()))]
+        traject = Traject(start_station)
+
+        #add stop
+        while True:
+            tried_conn = []
+            connect_name = random.choice(list(traject._endstation._connection.keys()))
+            connection = traject._endstation._connection[connect_name]
+            if not {traject._endstation._name, connect_name} in traject._trajectconnection:
+                if int(traject._traveltime) + int(connection[1]) > 120:
+                    break
+                traject.add_trajectconnection(connection[0])
+                tried_conn = []
+            elif connect_name not in tried_conn:
+                tried_conn.append(connect_name)
+            elif len(tried.conn) == len(traject._endstation._connection):
+                break
+    
+    def run_random_restr2_algoritme(self, n : int):
+    """
+    Maakt n aantal random trajecten
+    """
+
+    #Initialiseer parameters
+    lijst_traj = []
+
+    #Maakt n random trajecten
+    for i in range(n):
+        traject = self.random_restr2_traject()
+        lijst_traj.append(traject)
+
+    #Return
+    return lijst_traj
+
+def run_random_restr2_times(self, i : int):
+    """
+    Maakt i keer 1-7 random trajecten en returnt een lijst van nuttige resultaten:
+    [0]: een lijst met alle scores
+    [1]: de maximaal gehaalde score
+    [2]: het traject dat de maximale score heeft gehaald
+    [3]: de minimaal gehaalde score
+    [4]: het traject van minimale score
+    [5]: de tijd die dit algoritme heeft gerunt
+    """
+
+    #Initialiseert parameters
+    time0 = time.time()
+    score_list = []
+    max_score = 0
+    min_score = 10000
+    m = random.randint(1, 7)
+
+    #Runt algoritme i keer
+    for n in range(i):
+        traj = self.run_random_restr2_algoritme(m)
+        score_list.append(score_calc(traj))
+        if score_calc(traj) > max_score:
+            max_score = score_calc(traj)
+            max_traj = traj
+        elif score_calc(traj) < min_score:
+            min_score = score_calc(traj)
+            min_traj = traj
+    
+    #Berekent laatste waardes
+    time1 = time.time()
+
+    #Return
+    return [score_list, max_score, max_traj, min_score, min_traj, time1 - time0]
