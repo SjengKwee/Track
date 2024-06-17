675e0a3d763b5de9848f6b111723a9b61e638fde
addeym <addey.mugtaar@hva.nl>
2024-06-17 13:55:43 +0200

    voor hillclimber 2de wijziging toegevoegd en getest

--- code/algoritmes/hillClimber.py
+++ code/algoritmes/hillClimber.py
@@ -3,9 +3,11 @@ import random
 from code.bouwblokjes.score import *
 from code.classes.station import *
 from code.classes.traject import *
+from code.bouwblokjes.inladen import *
 
 
 
+random.seed(10)
 
 
 class HillClimber():
@@ -17,42 +19,89 @@ class HillClimber():
         
     
 
-    def mutate_trajectories(self, new_trajectories, number_of_mutation=1):
+    def mutate_trajectories(self, new_trajectories):
 
         #print(len(new_trajectories), "voor ")
+        # kies een getal en verwijder die traject 
         getal = random.randint(0, len(new_trajectories)-1)
         new_trajectories.pop(getal)
         print(getal,len(new_trajectories))
 
         #print(len(new_trajectories), "na ")
+    def delete_last_connection(self, new_trajectories):
+        
+        getal = random.randint(0, len(new_trajectories)-1)
+        old_traject = new_trajectories[getal]
+        start_station = new_trajectories[getal]._stations[0]
+        station_object = make_connections()
+        new_traject = Traject(station_object[start_station])
+       
+        for i in old_traject._stations[1:len(old_traject._stations)-1]:
+            new_traject.add_trajectconnection(station_object[i])
+
+        new_trajectories[getal] = new_traject
+        
+
+    
+      
+
+
+
+
+
+
+        verbindingen= len(new_trajectories[getal]._stations)
+        # print(len(new_trajectories))
+
+        # while verbindingen < 1:
+        #     getal = random.randint(0, len(new_trajectories)-1)
+        #     verbindingen= len(new_trajectories[getal]._stations)
+        
+        # station_verwijderen = new_trajectories[getal]._stations.pop()
+        # station_verwijderen = new_trajectories[getal]._trajectconnection.pop()
+        # print(new_trajectories[getal]._stations)
+        #return new_trajectories
+        #print(len(new_trajectories[getal]._stations),getal)
+
+
 
     def check_solution(self, new_trajectories):
+        #print(new_trajectories)
         new_value = score_calc(new_trajectories)
+        
         old_value = self.value 
+        #print(new_value, old_value)
     
         if new_value >= old_value:
             self.trajectories= new_trajectories
             self.value = new_value
+           
 
 
     
     def run(self, iterations, mutate_trajectories_number=1):
 
         # herhaal
+        
        
         for iteration in range(iterations):
+            
             print(self.value)
-
 
             new_trajectories = copy.deepcopy(self.trajectories)
             # doe een kleine random aanpassing 
             # een traject meer of minder rijden 
-            # een verbinding verwijderen 
-            # een verbinding toevoegen 
+            # van een traject laatste verbinding verwijderen 
+            # van een traject een verbinding toevoegen 
             # een traject bij een andere startpunt laten beginnen  
-            self.mutate_trajectories(new_trajectories, number_of_mutation=mutate_trajectories_number)
+            # een traject opsplitsten
+            self.mutate_trajectories(new_trajectories)
+            self.delete_last_connection(new_trajectories)
+            #print(new_trajectories)
+
             #als de state is verslechterd:
             self.check_solution(new_trajectories)
-        print(new_trajectories)
+        
+        print(new_trajectories, len(new_trajectories))
                 
                 
