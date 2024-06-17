4d346b05b3e9dd37a7426ed5ab7bc9e4b6fb3205
Alec <alecvanduin@DESKTOP-R25CRHQ>
2024-06-17 11:11:33 +0200

    [Alec]: Random restricted nr 2 af

--- code/algoritmes/random_restricted_2.py
+++ code/algoritmes/random_restricted_2.py
@@ -2,6 +2,13 @@
 # Random_algoritme_2.py
 # Hier definieren we een restricted random algoritme, waar tracks niet twee keer door dezelfde verbinding gaat
 
+import copy
+import time
+import random
+from code.classes.traject import *
+from code.classes.station import *
+from code.bouwblokjes.score import *
+
 class Restricted_2():
 
     def __init__(self, stations: dict):
@@ -11,10 +18,10 @@ class Restricted_2():
         #Initialiseer parameters
         start_station = self._stations[random.choice(list(self._stations.keys()))]
         traject = Traject(start_station)
+        tried_conn = []
 
         #add stop
         while True:
-            tried_conn = []
             connect_name = random.choice(list(traject._endstation._connection.keys()))
             connection = traject._endstation._connection[connect_name]
             if not {traject._endstation._name, connect_name} in traject._trajectconnection:
@@ -24,56 +31,58 @@ class Restricted_2():
                 tried_conn = []
             elif connect_name not in tried_conn:
                 tried_conn.append(connect_name)
-            elif len(tried.conn) == len(traject._endstation._connection):
+            elif len(tried_conn) == len(traject._endstation._connection):
                 break
+        
+        return traject
     
     def run_random_restr2_algoritme(self, n : int):
-    """
-    Maakt n aantal random trajecten
-    """
+        """
+        Maakt n aantal random trajecten
+        """
 
-    #Initialiseer parameters
-    lijst_traj = []
+        #Initialiseer parameters
+        lijst_traj = []
 
-    #Maakt n random trajecten
-    for i in range(n):
-        traject = self.random_restr2_traject()
-        lijst_traj.append(traject)
+        #Maakt n random trajecten
+        for i in range(n):
+            traject = self.random_restr2_traject()
+            lijst_traj.append(traject)
 
-    #Return
-    return lijst_traj
+        #Return
+        return lijst_traj
 
-def run_random_restr2_times(self, i : int):
-    """
-    Maakt i keer 1-7 random trajecten en returnt een lijst van nuttige resultaten:
-    [0]: een lijst met alle scores
-    [1]: de maximaal gehaalde score
-    [2]: het traject dat de maximale score heeft gehaald
-    [3]: de minimaal gehaalde score
-    [4]: het traject van minimale score
-    [5]: de tijd die dit algoritme heeft gerunt
-    """
+    def run_random_restr2_times(self, i : int):
+        """
+        Maakt i keer 1-7 random trajecten en returnt een lijst van nuttige resultaten:
+        [0]: een lijst met alle scores
+        [1]: de maximaal gehaalde score
+        [2]: het traject dat de maximale score heeft gehaald
+        [3]: de minimaal gehaalde score
+        [4]: het traject van minimale score
+        [5]: de tijd die dit algoritme heeft gerunt
+        """
 
-    #Initialiseert parameters
-    time0 = time.time()
-    score_list = []
-    max_score = 0
-    min_score = 10000
-    m = random.randint(1, 7)
+        #Initialiseert parameters
+        time0 = time.time()
+        score_list = []
+        max_score = 0
+        min_score = 10000
+        m = random.randint(1, 7)
 
-    #Runt algoritme i keer
-    for n in range(i):
-        traj = self.run_random_restr2_algoritme(m)
-        score_list.append(score_calc(traj))
-        if score_calc(traj) > max_score:
-            max_score = score_calc(traj)
-            max_traj = traj
-        elif score_calc(traj) < min_score:
-            min_score = score_calc(traj)
-            min_traj = traj
+        #Runt algoritme i keer
+        for n in range(i):
+            traj = self.run_random_restr2_algoritme(m)
+            score_list.append(score_calc(traj))
+            if score_calc(traj) > max_score:
+                max_score = score_calc(traj)
+                max_traj = traj
+            elif score_calc(traj) < min_score:
+                min_score = score_calc(traj)
+                min_traj = traj
     
-    #Berekent laatste waardes
-    time1 = time.time()
+        #Berekent laatste waardes
+        time1 = time.time()
 
-    #Return
-    return [score_list, max_score, max_traj, min_score, min_traj, time1 - time0]
+        #Return
+        return [score_list, max_score, max_traj, min_score, min_traj, time1 - time0]
--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 from code.algoritmes.random_algoritme import *
 from code.algoritmes.random_restricted import *
+from code.algoritmes.random_restricted_2 import *
 from code.bouwblokjes.plot_trajectories import *
 from code.bouwblokjes.inladen import *
 from code.bouwblokjes.score import *
 from code.bouwblokjes.writer import *
 import sys
 
+random.seed(10)
+
 stations = make_connections()
 
-print("Welkom bij dit vette ")
+print("Welkom bij onze vette algoritmes")
 print("Om onze random baseline te runnen: Random")
 print("Om de restricted random nr 1 te runnen: Restricted_1")
-algoritme = input("Welk algoritme wil je runnen?")
+print("Om de restricted random nr 2 te runnen: Restricted_2")
+algoritme = input("Welk algoritme wil je runnen? ")
 
 #Random algoritme
 if(algoritme == "Random"):
@@ -25,13 +29,24 @@ if(algoritme == "Random"):
 
 #Restricted nr 1
 elif(algoritme == "Restricted_1"):
-    restr1_results =run_random_restr_times(stations,10000)
+    restr1_results = run_random_restr_times(stations,10000)
     print("Het algoritme duurt", restr1_results[5], "seconden")
     run_plot_random_alg_score(restr1_results[0], 'data/images/random_restricted_1/scores_random_algoritme')
     tracks_writer(restr1_results[2], restr1_results[1], 'data/output/random_restricted_1/maximum.csv')
     tracks_writer(restr1_results[4], restr1_results[3], 'data/output/random_restricted_1/minimum.csv')
     run_plot_trajectories('data/output/random_restricted_1/maximum.csv', 'data/images/random_restricted_1/max_7_track.png')
     run_plot_trajectories('data/output/random_restricted_1/minimum.csv', 'data/images/random_restricted_1/min_7_track.png')
+
+#Restricted nr 2
+elif(algoritme == "Restricted_2"):
+    alg = Restricted_2(stations)
+    restr2_results = alg.run_random_restr2_times(10000)
+    print("Het algoritme duurt", restr2_results[5], "seconden")
+    run_plot_random_alg_score(restr2_results[0], 'data/images/random_restr_2/scores_algoritme')
+    tracks_writer(restr2_results[2], restr2_results[1], 'data/output/random_restr_2/maximum.csv')
+    tracks_writer(restr2_results[4], restr2_results[3], 'data/output/random_restr_2/minimum.csv')
+    run_plot_trajectories('data/output/random_restr_2/maximum.csv', 'data/images/random_restr_2/max_7_track.png')
+    run_plot_trajectories('data/output/random_restr_2/minimum.csv', 'data/images/random_restr_2/min_7_track.png')
 
 else:
     print("Verkeerde input")
